b4975ac690b61396ab85d23e980fd65ed1ec88cb
Shivam Kumar Sah <sahfamily91@gmail.com>
2023-08-15 10:06:24 +0530

    chnaged mongoURI for deployment

--- config/db.js
+++ config/db.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 const config = require('config');
-const db = config.get('mongoURI');
+// const db = config.get('mongoURI');
+
+const mongoURI = process.env.MONGO_URI;
+const db = mongoURI;
 
 const connectDB = async () => {
     try {
